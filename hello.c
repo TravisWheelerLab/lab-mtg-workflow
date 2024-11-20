4af290bdad2fc09869502b9a0adb2d1cc7c5cacf
Terrill Yuhas <tyuhas@arizona.edu>
2024-11-20 08:07:27 -0700

    Add sample file and workflow files

---
+++ hello.c
@@ -0,0 +1,11 @@
+// Header file for input output functions
+#include <stdio.h>
+
+// Main function: entry point for execution
+int main() {
+
+    // writing print statement to print hello
+    printf("Hello");
+
+    return 0;
+}
